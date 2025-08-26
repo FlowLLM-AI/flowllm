e0b8126e5db0a713350860b66cad4b9807606d8c
jinli.yl <jinli.yl@alibaba-inc.com>
2025-08-27 00:00:40 +0800

    feat(storage): implement experience utility management - Add new methods to update frequency and utility of experiences - Implement utility-based deletion for experiences - Update gallery to include DeepSearchToolFlow

--- flowllm/flow/gallery/__init__.py
+++ flowllm/flow/gallery/__init__.py
@@ -1,6 +1,7 @@
 from flowllm.flow.gallery.cmd_flow import CmdFlow
 from flowllm.flow.gallery.code_tool_flow import CodeToolFlow
 from flowllm.flow.gallery.dashscope_search_tool_flow import DashscopeSearchToolFlow
+from flowllm.flow.gallery.deepsearch_tool_flow import DeepSearchToolFlow
 from flowllm.flow.gallery.expression_tool_flow import ExpressionToolFlow
 from flowllm.flow.gallery.mock_tool_flow import MockToolFlow
 from flowllm.flow.gallery.tavily_search_tool_flow import TavilySearchToolFlow
