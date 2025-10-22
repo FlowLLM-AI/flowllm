510bd1d316ba478b62d09bd64d1656fa48cbda70
jinli.yl <jinli.yl@alibaba-inc.com>
2025-10-22 19:55:26 +0800

    chore(version): bump version to 0.11.2

--- flowllm/__init__.py
+++ flowllm/__init__.py
@@ -2,7 +2,7 @@ from flowllm.utils.common_utils import load_env
 
 load_env()
 
-__version__ = "0.1.11.1"
+__version__ = "0.1.11.2"
 
 from flowllm.app import FlowLLMApp
 from flowllm.op import BaseOp, BaseAsyncOp, BaseAsyncToolOp, BaseMcpOp, BaseRayOp
