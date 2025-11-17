bc6f67b8893919a4337ca05b6d073da0a16e0e3d
jinli.yl <jinli.yl@alibaba-inc.com>
2025-11-17 12:09:10 +0800

    feat(utils): add date/time utility functions for FlowLLM extensions

--- flowllm/extensions/__init__.py
+++ flowllm/extensions/__init__.py
@@ -5,11 +5,13 @@ It includes ready-to-use extension packages for:
 
 - file_tool: File-related operations including editing and searching files
 - data: Data-related operations including downloading stock data
+- utils: Utility functions for date/time operations and other helpers
 """
 
-from . import data, file_tool
+from . import data, file_tool, utils
 
 __all__ = [
     "data",
     "file_tool",
+    "utils",
 ]
